b15d3ab3d545d48e0e187c57074580b0046cecfe
huonghocweb <huongpham1913@gmail.com>
2026-03-30 10:34:38 +0700

    feat(user): implement CRUD REST API for users #1

--- workingsystem/src/main/java/com/huong/workingsystem/api/UserApi.java
+++ workingsystem/src/main/java/com/huong/workingsystem/api/UserApi.java
@@ -33,6 +33,77 @@ import org.springframework.web.multipart.MultipartFile;
 @CrossOrigin("*")
 @RequestMapping("/api/users")
 public class UserApi {
+    @Autowired
+    UserService userService;
 
+    @GetMapping("/v1")
+    public ResponseEntity<Object> getAllUser(
+            @RequestParam("page") Integer pageNumber,
+            @RequestParam("size") Integer pageSize,
+            @RequestParam("by") String sortBy,
+            @RequestParam("order") String sortOrder
+           ) {
+        System.out.println("get all user:  " + pageNumber + pageSize + sortBy + sortOrder);
+        Direction sortDirection = sortOrder.equalsIgnoreCase("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(sortDirection, sortBy);
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+     //   System.out.println(userService.getAllUser(pageable));
+        ApiResponse<Object> response = ApiResponse.builder()
+                .success(true)
+                .message("Get All User successful")
+                .data(userService.getAllUser(pageable))
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/v1/{userId}")
+    public ResponseEntity<ApiResponse<Object>> getUserByUserId(
+            @PathVariable("userId") Integer userId) {
+        //System.out.println("userId: " + userId);
+        ApiResponse<Object> response = ApiResponse.builder()
+                .success(true)
+                .message("Get user by id success")
+                .data(userService.getUserByUserId(userId))
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/v1")
+    public ResponseEntity<Object> createUser(
+            @RequestPart("userRequest") UserRequest userRequest,
+            @RequestPart(value = "files" , required = false) MultipartFile[] files
+            ) throws IOException {
+        Map<String, Object> result = new HashMap<>();
+        System.out.println("createUser:  " + userRequest);
+//        System.out.println("File:  "  + Arrays.toString(files));
+//        System.out.println("FileSIze:" + files[0].getSize());
+        ApiResponse<Object> responsse =  ApiResponse.builder()
+                .success(true)
+                .data(userService.createUser(userRequest, files))
+                .message("Create User success")
+                .build();
+        return ResponseEntity.ok(responsse);
+    }
+
+    @PutMapping("/v1/{userId}")
+    public ResponseEntity<Object> updateUser(
+            @PathVariable("userId") Integer userId,
+            @RequestPart("userRequest") UserRequest userRequest ,
+            @RequestPart(value = "files", required = false) MultipartFile[] files
+    ) {
+        Map<String, Object> result = new HashMap<>();
+        System.out.println("updateUser:  " + userRequest);
+       // System.out.println("files: " + files[0].getSize());
+        try {
+            result.put("success", true);
+            result.put("data", userService.updateUser(userId, userRequest , files));
+            result.put("message", "Update User");
+        } catch (Exception e) {
+            result.put("success", false);
+            result.put("data", null);
+            result.put("message", e.getMessage());
+        }
+        return ResponseEntity.ok(result);
+    }
 
 }
--- workingsystem/src/main/java/com/huong/workingsystem/serviceImpl/UserServiceImpl.java
+++ workingsystem/src/main/java/com/huong/workingsystem/serviceImpl/UserServiceImpl.java
@@ -28,5 +28,78 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private RoleRepo roleRepo;
+    @Autowired
+    private CloudinaryService cloudinaryService;
 
+    @Override
+    //readOnly : tối ưu hóa hiệu năng truy vấn , chặn khi có thay đổi db
+    @Transactional(readOnly = true)
+    public PageResponse<UserResponse> getAllUser(Pageable pageable) {
+        long totalElements = userRepo.count();
+        int  actualTotalPage = (int) Math.ceil((double) totalElements / pageable.getPageSize());
+
+        if (pageable.getPageNumber()  >= actualTotalPage) {
+            pageable= PageRequest.of(actualTotalPage -1, pageable.getPageSize(), pageable.getSort()  );
+        }
+        Page<User> usersPage = userRepo.findAll(pageable);
+        List<UserResponse> userResponse = usersPage.getContent().stream()
+        .map(userMapper :: convertEnToRes)
+        .collect(Collectors.toList());
+
+        return new PageResponse<UserResponse>(usersPage,  userResponse);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserResponse getUserByUserId(Integer userId) {
+        User user = userRepo.findById(userId)
+        .orElseThrow(() -> new EntityNotFoundException("not found User"));
+        UserResponse userResponse = userMapper.convertEnToRes(user);
+        userResponse.setImageUrl(cloudinaryService.getImageUrl(userResponse.getImagePublicId()));
+        return userResponse;
+    }
+
+    @Override
+    @Transactional
+    public UserResponse getUserByUserName(String userName) {
+        User user = userRepo.getUserByUserName(userName);
+        return userMapper.convertEnToRes(user);
+    }
+
+    @Transactional
+    @Override
+    public UserResponse createUser(UserRequest userRequest, MultipartFile[] files) throws IOException {
+        System.out.println("UserReq: " + userRequest);
+        User user = userMapper.convertReqToEn(userRequest);
+      //  user.setPassword();
+      user.setRoles( userRequest.getRoleIds()!= null ?
+              roleRepo.findAllById(userRequest.getRoleIds()) : null);
+      // saveRoleId nưa
+        if(files != null)
+        user.setImagePublicId(cloudinaryService.uploadFile(files,  "user").get(0));
+        User newUser = userRepo.save(user);
+        return userMapper.convertEnToRes(newUser);
+    }
+
+
+    @Override
+    public UserResponse updateUser(Integer userId, UserRequest userRequest , MultipartFile[] files) throws IOException{
+        User userExists = userRepo.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException("not found user to update"));
+        // map từ userRequest sang user nhưng sẽ là update các trường khac null trong userRequest
+        userMapper.updateEntityFromRequest(userRequest, userExists);
+        if(files != null) {
+            userExists.setImagePublicId(cloudinaryService.uploadFile(files ,"user").get(0));
+        }
+        if (userRequest.getRoleIds() != null) {
+            userExists.setRoles(roleRepo.findAllById(userRequest.getRoleIds()));
+        }
+        return userMapper.convertEnToRes(userRepo.save(userExists));
+    }
 }
